ee78e0a2e5afd114acaf6a8a4bd37d5a85344f80
Varun Sampath <vsampath@seas.upenn.edu>
2011-11-17 16:04:11 -0500

    added web_settings_template and updated README

---
+++ uWaveArduino/web_settings_template.h
@@ -0,0 +1,8 @@
+#ifndef _WEB_SETTINGS_H_
+#define _WEB_SETTINGS_H_
+byte mac[] = { 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA }; // Arduino MAC Address
+byte server[] = { 127, 0, 0, 1 }; // Server
+byte ip[] = { 127, 0, 0, 1 }; // Arduino IP
+#endif
+
+#endif // #ifndef _WEB_SETTINGS_H_
